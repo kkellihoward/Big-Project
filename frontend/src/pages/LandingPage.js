d88a07b733339b65476cb9520f6e0c10c39ba86f
mattsterchief <47911061+mattsterchief@users.noreply.github.com>
2023-07-10 20:01:21 -0400

    Started on Login LoginModal

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import LandingBar from '../components/LandingBar';
+import { LoginModal } from '../components/LoginModal';
+import { Button } from '../components/Button';
+import Image1 from '../components/image1';
+import Image2 from '../components/image2';
+import '../components/Button.css';
 
 function LandingPage()
 {
+    const [showModal, setShowModal] = useState(false);
+
+    const openModal = () => {
+        setShowModal(prev => !prev);
+    };
+
+    const onClick = () => {
+        
+    }
+
     return(
         <>
             <LandingBar />
+            <LoginModal showModal={showModal} setShowModal={setShowModal}/>
+            <Image1/>
+            <Image2/>
+            <Button onClick={onClick}>Get Started</Button>
         </>
     );
 };
+
 
 export default LandingPage;
