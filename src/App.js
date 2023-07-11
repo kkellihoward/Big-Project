7ad29a1ee088c210de56e920a6371e6b677b50b1
kkellihoward <89551357+kkellihoward@users.noreply.github.com>
2023-07-11 00:36:24 -0400

    Update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from 'react';
-import LandingPage from './pages/LandingPage';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <LandingPage />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
