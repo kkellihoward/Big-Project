25e815b6fa674deb29b0ea63508bdf21642aa95c
kkellihoward <89551357+kkellihoward@users.noreply.github.com>
2023-07-11 11:35:09 -0400

    Updated  Login and Sign up Modal

--- src/components/LoginModal.js
+++ src/components/LoginModal.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import * as Components from './LoginSignup';
 import { MdClose } from 'react-icons/md';
 
 const Background = styled.div`
@@ -17,7 +18,7 @@ const ModalWrapper = styled.div`
   height: 500px;
   box-shadow: 0 5px 16px rgba(0, 0, 0, 0.2);
   background: #fff;
-  color: #000;
+  color: black;
   display: grid;
   grid-template-columns: 1fr 1fr;
   position: relative;
@@ -46,13 +47,61 @@ const CloseModalButton = styled(MdClose)`
 `;
 
 export const LoginModal = ({ showModal, setShowModal }) => {
+  const [signIn, toggle] = React.useState(true);
   return (
     <>
       {showModal && (
         <Background>
           <ModalWrapper showModal={showModal}>
             <ModalContent>
-              <h1>It's Modal Time!</h1>
+            <Components.Container>
+              <Components.SignUpContainer signinIn={signIn}>
+                  <Components.Form>
+                      <Components.Title>Create Account</Components.Title>
+                      <Components.Input type='text' placeholder='Name' />
+                      <Components.Input type='email' placeholder='Email' />
+                      <Components.Input type='password' placeholder='Password' />
+                      <Components.Button style={{backgroundColor: '#7f44d4'}}>Sign Up</Components.Button>
+                  </Components.Form>
+              </Components.SignUpContainer>
+
+              <Components.SignInContainer signinIn={signIn}>
+                   <Components.Form>
+                       <Components.Title>Sign in</Components.Title>
+                       <Components.Input type='email' placeholder='Email' />
+                       <Components.Input type='password' placeholder='Password' />
+                       <Components.Anchor href='#'>Forgot your password?</Components.Anchor>
+                       <Components.Button style={{backgroundColor: '#7f44d4'}}>Sigin In</Components.Button>
+                   </Components.Form>
+              </Components.SignInContainer>
+
+              <Components.OverlayContainer signinIn={signIn}>
+                  <Components.Overlay signinIn={signIn}>
+
+                  <Components.LeftOverlayPanel signinIn={signIn}style={{backgroundColor: '#7f44d4'}}>
+                      <Components.Title>Welcome Back!</Components.Title>
+                      <Components.Paragraph>
+                          To keep connected with us please login with your personal info
+                      </Components.Paragraph>
+                      <Components.GhostButton onClick={() => toggle(true)}>
+                          Sign In
+                      </Components.GhostButton>
+                      </Components.LeftOverlayPanel>
+
+                      <Components.RightOverlayPanel signinIn={signIn} style={{backgroundColor: '#7f44d4'}}>
+                        <Components.Title>Hello, Friend!</Components.Title>
+                        <Components.Paragraph>
+                            Enter Your personal details and start journey with us
+                        </Components.Paragraph>
+                            <Components.GhostButton onClick={() => toggle(false)}>
+                                Sign Up
+                            </Components.GhostButton> 
+                      </Components.RightOverlayPanel>
+  
+                  </Components.Overlay>
+              </Components.OverlayContainer>
+
+          </Components.Container>
             </ModalContent>
             <CloseModalButton
                 aria-label='Close modal'
