e0a93d69fee63e8d5d6bec3a3d1bdfaba581293c
lmcarroll987 <137725867+lmcarroll987@users.noreply.github.com>
2023-07-18 18:35:18 -0400

    Update server.js

--- API/server.js
+++ API/server.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
-const accountRoutes = require('./routes/accounts');
+const accountRoutes = require('./accounts');
+const eventRoutes = require('./events');
 
 // starts up express app
 const app = express();
@@ -16,7 +17,8 @@ app.use((req, res, next) => {
 });
 
 // routes
-app.use('/api/accounts', accountRoutes);
+app.use('/accounts', accountRoutes);
+app.use('/events', eventRoutes);
 
 // connect to database
 mongoose.connect(process.env.MONGO_URI)
