ff3c6b7c8c541b84d29fba5c71134d4d1b96c3f7
kkellihoward <89551357+kkellihoward@users.noreply.github.com>
2023-07-19 20:20:21 -0400

    Trying to mke the stupid calls work

--- src/components/LoginModal.js
+++ src/components/LoginModal.js
@@ -72,17 +72,17 @@ export const LoginModal = ({ showModal, setShowModal}) => {
     console.log(tempUser.password);
     // post to url where api server
     axios.get('https://bp-api-87a503314fa5.herokuapp.com/accounts/login', tempUser)
-    // .then( response =>
-    //   {
+    .then( response =>
+      {
         
-    //     if(response.status == 200)
-    //     {
-    //       console.log(response);
-    //       // navigate('/Events')
-    //     }
-    //     console.log("here");
-    //     }   
-    // )
+        if(response.status == 200)
+        {
+          console.log(response);
+          // navigate('/Events')
+        }
+        console.log("here");
+        }   
+    )
     
      .catch( err => console.log(err))
   };
