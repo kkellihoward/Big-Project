cd8330a0aa71281d4a2bb2cf296d9159d4e389e5
kkellihoward <89551357+kkellihoward@users.noreply.github.com>
2023-07-21 20:37:30 -0400

    updating login api call

--- src/components/LoginModal.js
+++ src/components/LoginModal.js
@@ -67,7 +67,7 @@ export const LoginModal = ({ showModal, setShowModal}) => {
 
     try {
       const apiUrl = 'https://bp-api-87a503314fa5.herokuapp.com/user/signin'; // Replace with your API endpoint
-      const data = { email, password };
+      const data = { user, password };
   
       const response = await axios.post(apiUrl, data, { headers: {
         'Content-Type': 'application/json'
