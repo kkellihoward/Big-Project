17c904c9f2839f10210668e0f289982308f788bc
kkellihoward <89551357+kkellihoward@users.noreply.github.com>
2023-08-03 05:31:00 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { BrowserRouter, Routes, Route} from 'react-router-dom';
 //pages
 import LandingPage from './pages/LandingPage';
 import EventsPage from './pages/EventPage';
+import ResetPass from './pages/ResetPass';
+import VerificationPage from './pages/VerificationPage';
 import SpecificEventPage from './pages/SpecificEventPage';
 
 function App() {
@@ -16,6 +18,8 @@ function App() {
           <Route path='/' element ={<LandingPage />} />
           <Route path='Events' element ={<EventsPage />} />
           <Route path= 'Sleepover' element = {<SpecificEventPage />} />
+          <Route path= '/verify-email/:token' element = {<VerificationPage />} />
+          <Route path= '/reset-password/:token' element = {<ResetPass />} />
         </Routes>
       </main>
     </BrowserRouter>
